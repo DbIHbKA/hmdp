25691629205530ea2b7c0d8d8c9c4a16f1c77b2e
Danila <ddbihbka@gmail.com>
2015-08-08 09:35:57 +0300

    Restyle and fix bug

--- src/MDP.hs
+++ src/MDP.hs
@@ -5,9 +5,9 @@ module MDP where
 import Data.Set (Set)
 
 data MDP s a = MDP
-  { states :: Set s             -- ^ is finite set of state
-  , actions :: Set a            -- ^ is finite set of actions
-  , transition :: a -> s -> s -> Double -- ^ is a state transition probability matrix
-  , reward :: a -> s -> Double          -- ^ is a reward function
-  , gamma :: Double                     -- ^ is a discount factor
-  }
+    { states :: Set s             -- ^ is finite set of state
+    , actions :: Set a            -- ^ is finite set of actions
+    , transition :: a -> s -> s -> Double -- ^ is a state transition probability matrix
+    , reward :: a -> s -> Double          -- ^ is a reward function
+    , gamma :: Double                     -- ^ is a discount factor
+    }
--- src/Planning.hs
+++ src/Planning.hs
@@ -16,12 +16,17 @@ policyEvaluation :: MDP s a -> ValueFunction s a
 policyEvaluation mdp policy = evalVF
   where
     evalVF 0 _ = 0.0
-    evalVF k s = sum (
-      map (
-        \a ->
-           policy s a * (reward mdp a s + gamma mdp * sum (
-             map (\s' ->
-               transition mdp a s s' * (evalVF (k-1) s')) (S.toList (states mdp)))
-                 )
-                   )
-                     (S.toList (actions mdp)))
+    evalVF k s =
+        sum
+            (map
+                 (\a ->
+                       policy s a *
+                       (reward mdp a s +
+                        gamma mdp *
+                        sum
+                            (map
+                                 (\s' ->
+                                       transition mdp a s s' *
+                                       evalVF (k - 1) s')
+                                 (S.toList (states mdp)))))
+                 (S.toList (actions mdp)))
